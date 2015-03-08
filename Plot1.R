712d932c1385821df7e970a30f2d70705dd47142
montserrat_perez <montserrat@spanadvisory.com>
2015-03-08 13:23:41 +0000

    I hope it works

---
+++ Plot1.R
@@ -0,0 +1,28 @@
+ hpc <- read.csv("householdpowerconsumption.txt", sep = ";", header = TRUE,
+                  na.strings ="?", stringsAsFactors=F) #to read the file
+ 
+  hpc$DateTime <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S") 
+  
+  #to merge date and time columns and convert them to posixlt format
+  
+  myfunc <- function (x, y){
+          hpc[hpc$DateTime >= x & hpc$DateTime <= y,  ]
+          
+          }
+  # to be able to subset data frame by date range
+  
+  Test <-myfunc ("2007-02-01 00:00:00", "2007-02-02 23:59:59 ")
+  
+  # to subset data frame by given dates
+ 
+ hist (Test$Global_active_power, main= "Global Active Power", 
+       xlab = "Global Active Power (kilowatts)", col = "red")
+ 
+ #to call up the histogramm
+ 
+ dev.copy(png, "GlobalActivePower.png", width =480, height = 480)
+  
+ #to print the histogram to png format
+ 
+ dev.off()
+ # to close png device
